23d51e7ec4f4dafbacde506e1505cbb124909e71
sash <sash2005@rambler.ru>
2016-10-12 22:27:40 +0500

    В файле import_product открываю файл csv для чтения.

--- shop/management/commands/import_product.py
+++ shop/management/commands/import_product.py
@@ -1,3 +1,5 @@
+import csv
+
 from django.core.management.base import BaseCommand
 from shop.models import Product, Category
 
@@ -21,20 +23,68 @@ class Command(BaseCommand):
         :return:
         """
 
-        # Пример как создаётся (сохраняется) категория
-        category = Category()
-        category.name = 'Электровеники'
-        category.slug = 'myvenik'
-        category.save()
+        check_goods = False
+        file = open('myshop.csv', 'r')
+        product = Product.objects.all()
+        for line in file:
+            arr = line.split(';')
+            for p in product:
+                print(p.name)
+                if arr[0] == p.name:
+                    print('Совпало')
+                    check_goods = True
+            if not check_goods:
+                print('check')
+                creator(arr[9])
+            check_goods = False
+            #print(arr[1])
+        #file.close()
 
-        # и товар
-        product = Product()
-        product.name = 'test1'
-        product.slug = 'mytest'
-        product.price = 100
-        product.stock = 12
-        product.available = True
-        product.category = category
-        product.save()
 
         self.stdout.write('Successfully')
+
+
+
+def creator(category_name):
+    category = Category.objects.all()
+    for c in category:
+        print(c.name)
+        print(category_name)
+        if c.name == category_name:
+            print( c.name)
+            print('Мониторы+Тв')
+            print('from for category ravno')
+        else:
+            print(c.name)
+            print(category_name)
+            print('from for category ne ravno')
+    #    category.name = 'Электровеники'
+    #    category.slug = 'myvenik'
+    #    category.save()
+#
+    #product = Product()
+    #product.name = 'test1'
+    #product.slug = 'mytest'
+    #product.price = 100
+    #product.stock = 12
+    #product.available = True
+    #product.category = category
+    #product.save()
+    #self.stdout.write('From creator')
+
+
+            # Пример как создаётся (сохраняется) категория
+            # category = Category()
+            # category.name = 'Электровеники'
+            # category.slug = 'myvenik'
+            # category.save()
+            #
+            ## и товар
+            # product = Product()
+            # product.name = 'test1'
+            # product.slug = 'mytest'
+            # product.price = 100
+            # product.stock = 12
+            # product.available = True
+            # product.category = category
+            # product.save()
